17ee7fd2591d1a00219c2b690920b916872feabb
Catherine Tower <CMallocTower@gmail.com>
2020-04-13 08:23:20 -0700

    Handle whitespace in filenames

--- other-utilities/plot-all-in-directory.bat
+++ other-utilities/plot-all-in-directory.bat
@@ -1,1 +1,1 @@
-for /r %%i in (*.dwg) do "C:\Program Files\Autodesk\AutoCAD 2013\accoreconsole.exe" /i %%i /s plot-file.scr
+for /r %%i in (*.dwg) do "C:\Program Files\Autodesk\AutoCAD 2013\accoreconsole.exe" /i "%%i" /s plot-file.scr
